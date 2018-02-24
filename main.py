91ba3f7214a477057ddb4349f6fa0d00e157b0f8
LukeBaal <luke.baal@uoit.net>
2018-02-23 19:48:03 -0500

    Removed dev code, fixed results return code in main.py

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ if __name__ == "__main__":
 
     game = Game()
 
-    # for cate in game.score:
-    #     game.score[cate]["scored"] = True
-    
-    # game.score["ones"]["scored"] = False
-    
     game.run_game()
 
-    result, total = game.get_current_score()
-    print_results(result, total)
+    result, subtotal, total = game.get_current_score()
+    print_results(result, subtotal, total)
